fad864913371845a50e276d60b80336ee2aaa497
Seyma CELEBI <seyma.celebi@st.uskudar.edu.tr>
2021-06-09 22:06:23 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Dashboard from './layouts/Dashboard';
- import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-css/semantic.min.css'
+import { Container } from 'semantic-ui-react';
+import Navi from './layouts/Navi';
+
+
 function App() {
   return (
     <div className="App">
-      <Dashboard></Dashboard>
+      <Navi/>
+      <Container className="main">
+         <Dashboard/>
+      </Container>    
     </div>
   );
 }
--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Navi from "./Navi";
 import Categories from "./Categories";
 import ProductList from "../pages/ProductList";
 export default function Dashboard() {
     return (
         <div>
-        <Navi/>
         <Categories/>
         <ProductList/>
         </div>
