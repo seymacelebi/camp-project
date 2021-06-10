7afa0f792915216f5509c2ae041106f9efa21602
Seyma CELEBI <seyma.celebi@st.uskudar.edu.tr>
2021-06-10 18:05:01 +0300

    12.ders sonu

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import CartSummary from "./CartSummary";
-import { Button, Container, Menu } from "semantic-ui-react";
+import { Container, Menu } from "semantic-ui-react";
+import SignedOut from "./SignedOut";
+import SignedIn from "./SignedIn";
+import { useHistory } from "react-router";
 
 export default function Navi() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
+  const history = useHistory()
+  function handleSignOut() {
+    setIsAuthenticated(false)
+    history.push("/")
+  }
+
+  function handleSignIn() {
+    setIsAuthenticated(true)
+  }
+
   return (
     <div>
       <Menu inverted fixed="top">
@@ -12,9 +26,8 @@ export default function Navi() {
 
           <Menu.Menu position="right">
             <CartSummary/>
-            <Menu.Item>
-              <Button primary>Sign Up</Button>
-            </Menu.Item>
+            {isAuthenticated?<SignedIn signOut={handleSignOut} bisey="1"/>
+            :<SignedOut signIn={handleSignIn}/>}  
           </Menu.Menu>
         </Container>
       </Menu>
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import React, { useState,useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Icon, Label, Menu, Table } from "semantic-ui-react";
 import ProductService from "../services/productService";
 
-
 export default function ProductList() {
+  const [products, setProducts] = useState([]);
 
-const [products, setProducts] = useState([])
-useEffect(()=>{
-  let productService=new ProductService()
-  productService.getProducts().then(result=>setProducts(result.data.data))
-})
-    return (
-        <div>
+  useEffect(()=>{
+    let productService = new ProductService()
+    productService.getProducts().then(result=>setProducts(result.data.data))
+  },[])
+
+  return (
+    <div>
       <Table celled>
         <Table.Header>   
           <Table.Row>
@@ -22,11 +23,10 @@ useEffect(()=>{
             <Table.HeaderCell>Kategori</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
-
         <Table.Body>
           {products.map((product) => (
             <Table.Row key={product.id}>
-             <Table.Cell>{product.productName}</Table.Cell>
+              <Table.Cell><Link to={`/products/${product.productName}`}>{product.productName}</Link></Table.Cell>
               <Table.Cell>{product.unitPrice}</Table.Cell>
               <Table.Cell>{product.unitsInStock}</Table.Cell>
               <Table.Cell>{product.quantityPerUnit}</Table.Cell>
@@ -34,26 +34,26 @@ useEffect(()=>{
             </Table.Row>
           ))}
         </Table.Body>
-    <Table.Footer>
-      <Table.Row>
-        <Table.HeaderCell colSpan='3'>
-          <Menu floated='right' pagination>
-            <Menu.Item as='a' icon>
-              <Icon name='chevron left' />
-            </Menu.Item>
-            <Menu.Item as='a'>1</Menu.Item>
-            <Menu.Item as='a'>2</Menu.Item>
-            <Menu.Item as='a'>3</Menu.Item>
-            <Menu.Item as='a'>4</Menu.Item>
-            <Menu.Item as='a' icon>
-              <Icon name='chevron right' />
-            </Menu.Item>
-          </Menu>
-        </Table.HeaderCell>
-      </Table.Row>
-    </Table.Footer>
-  </Table>
 
-        </div>
-    )
+        <Table.Footer>
+          <Table.Row>
+            <Table.HeaderCell colSpan="3">
+              <Menu floated="right" pagination>
+                <Menu.Item as="a" icon>
+                  <Icon name="chevron left" />
+                </Menu.Item>
+                <Menu.Item as="a">1</Menu.Item>
+                <Menu.Item as="a">2</Menu.Item>
+                <Menu.Item as="a">3</Menu.Item>
+                <Menu.Item as="a">4</Menu.Item>
+                <Menu.Item as="a" icon>
+                  <Icon name="chevron right" />
+                </Menu.Item>
+              </Menu>
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Footer>
+      </Table>
+    </div>
+  );
 }
